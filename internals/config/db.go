9471ef815dab0364de9555313dfb501cc522578a
Natnael <nsisay49@gmail.com>
2026-07-09 21:08:02 +0300

    configuration for getting db url and connecting

--- internals/config/db.go
+++ internals/config/db.go
@@ -1,1 +1,34 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Db *gorm.DB
+
+func ConnectToDb() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("unable to load env file")
+		return
+	}
+
+	db_url := os.Getenv("DATABASE_URL")
+	if db_url == "" {
+		log.Fatal("no database url found")
+		return
+	}
+
+	conn, err := gorm.Open(postgres.Open(db_url), &gorm.Config{})
+	if err != nil {
+		log.Fatal("unable to connect to the db")
+		return
+	}
+
+	Db = conn
+}
