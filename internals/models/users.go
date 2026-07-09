857bb719f86f6318bc7ead1ff52714b34f704d02
Natnael <nsisay49@gmail.com>
2026-07-09 21:51:01 +0300

    model changes

--- internals/models/users.go
+++ internals/models/users.go
@@ -3,7 +3,7 @@ package models
 type User struct {
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
-	PhoneNumber string `json:"phone_number"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
+	PhoneNumber string `json:"phone_number" gorm:"unique"`
+	Email       string `json:"email" gorm:"unique;not null"`
+	Password    string `json:"password" gorm:"not null"`
 }
---
+++ internals/routes/authRoutes.go
@@ -0,0 +1,6 @@
+package routes
+
+import "github.com/gorilla/mux"
+
+func AuthRoutes(router mux.Router) {
+}
